f9f3b5509771ec3b7d1b78b1639249dba68c53f1
brendapagin <brendapagin@gmail.com>
2026-07-05 14:39:13 -0300

    Create PainelMarca.jsx

---
+++ src/components/PainelMarca.jsx
@@ -0,0 +1,30 @@
+export function PainelMarca() {
+  return (
+    <div className="painel-marca">
+      <div className="marca">
+        <svg viewBox="0 0 100 100">
+          <path
+            className="flor"
+            d="M50 30
+              C 50 30, 40 15, 50 5
+              C 60 15, 50 30, 50 30
+              M50 30
+              C 50 30, 30 25, 20 35
+              C 30 40, 50 30, 50 30
+              M50 30
+              C 50 30, 70 25, 80 35
+              C 70 40, 50 30, 50 30
+              M50 30
+              C 50 30, 35 45, 30 60
+              C 45 60, 50 30, 50 30
+              M50 30
+              C 50 30, 65 45, 70 60
+              C 55 60, 50 30, 50 30"
+          />
+        </svg>
+        <h1>GLOW</h1>
+        <p>Saúde e Beleza</p>
+      </div>
+    </div>
+  );
+}
